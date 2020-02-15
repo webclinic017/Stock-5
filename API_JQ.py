a99484266854bebd9c85f85d00d7a88463953c00
cj <sizhe.huang@guanyueinternational.com>
2020-02-15 17:53:37 +0800

    cleanup for desktop germany 2

---
+++ API_JQ.py
@@ -0,0 +1,14 @@
+import tushare as ts
+import pandas as pd
+import time
+import Util
+import traceback
+from jqdatasdk import *
+
+auth('13817373362', '373362')
+
+if __name__ == '__main__':
+    df = get_price('000001.XSHE', start_date='2015-01-01', end_date='2015-01-31 23:00:00', frequency='minute', fields=['open', "high", "low", 'close'])
+    print(df)
+    df.to_csv("testmindata.csv")
+    pass
