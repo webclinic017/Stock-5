cc4b1f361ed141d920afd666c8f0f507657da576
思哲 <sizhe.huang@guanyueinternational.com>
2020-05-01 09:39:28 +0800

    scrapy

--- _API_Scrapy.py
+++ _API_Scrapy.py
@@ -1,16 +1,32 @@
-import pysnowball as ball
-
-
-import pandas as pd
-import numpy as np
-import DB,LB
-
-
-
+import scrapy
 import requests
-response = requests.get('http://stock.finance.sina.com.cn/stock/go.php/vReport_List/kind/search/index.phtml?symbol=sz000651&t1=all&p=1')
-print (response.status_code)
-print (response.content)
-html=response.content.decode("ascii")
-print(html)
-print("格力电器" in html)
+import xlrd
+import xlsxwriter
+import time
+
+
+def start():
+    url = (time.strftime("%Y.%m.%d-%H%M%S."))
+    readworkbook = xlrd.open_workbook('quanshang.xlsx')
+    readworksheet = readworkbook.sheet_by_index(0)
+
+    print('START')
+
+    for i in range(readworksheet.nrows):
+        url = readworksheet.cell_value(i, 0)
+        print("url:", url)
+
+        sel = scrapy.Selector(text=requests.get(url).text)
+
+        # main = sel.xpath('//div[@class="main"]/table/tbody').extract()
+        s_titles = sel.xpath('//td[@class="tal f14"]/a/@title').extract()
+        s_date = sel.xpath('//td[@class="tal f14"]/following-sibling::td[2]/text()').extract()
+        s_qdii = sel.xpath('//td[@class="tal f14"]/following-sibling::td[3]/a/div/span/text()').extract()
+        s_person = sel.xpath('//td[@class="tal f14"]/following-sibling::td[4]/div/span/text()').extract()
+
+        for counter, (title, date, qdii, person) in enumerate(zip(s_titles, s_date, s_qdii, s_person)):
+            print(counter, title, date, qdii, person)
+
+
+if __name__ == "__main__":
+    start()
